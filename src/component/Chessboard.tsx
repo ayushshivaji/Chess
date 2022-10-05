9304e6de68fd26da66c64083e9cd83cbd2429e25
Ayush Srivastava <ayushclashroyale@gmail.com>
2022-10-06 01:06:44 +0530

    First Commit:v0.1

---
+++ src/component/Chessboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Chessboard.css';
+
+const horizontalAxis = ["a","b","c","d","e","f","g","h"]
+const verticalAxis = [1,2,3,4,5,6,7,8]
+
+export default function Chessboard(){
+    return <div id='Chessboard'> </div>
+}
